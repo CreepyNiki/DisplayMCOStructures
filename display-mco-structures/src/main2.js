bac5e464d7b0c02dc6f7c413064ca6782250072e
Niklas Halft <nlh352@gmail.com>
2026-06-11 16:17:19 +0200

    feat: first map version

--- display-mco-structures/src/main2.js
+++ display-mco-structures/src/main2.js
@@ -1,6 +1,42 @@
 
+import LatLon from 'geodesy/latlon-ellipsoidal.js';
+
+let map = L.map('map').setView([41.505, 12.49], 3);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+Promise.all([
+    fetch(new URL("/assets/MCOs.json", import.meta.url)).then(r => r.json()),
+    fetch(new URL("/assets/Clubs.json", import.meta.url)).then(r => r.json())
+]).then(([mcos, clubs]) => {
+
+    for (const club of clubs) {
+
+        let badgeIcon = L.icon({
+            iconUrl: club.badge,
+            iconSize: [64, 64, 64],
+        });
+
+        const coord = normalizeCoord(club.Koordinaten);
+        const point = LatLon.parse(coord);
+        L.marker([point.lat, point.lon], {
+            icon: badgeIcon,
+        }).addTo(map);
+    }
+});
+
+function normalizeCoord(raw) {
+    return raw
+        .trim()
+        .replace(/(\d),(\d)/g, '$1.$2')
+        .replace(/\bO\b/g, 'E')
+        .replace(/([NS])\s*([EW])/g, '$1, $2')
+        .replace(/\s*,\s*/g, ', ');
+}
+
 function showPopup(d) {
-    const popup = d3.select(".info-popup");
     if (d.type === "mco") {
         popup
             .classed("hidden", false)
